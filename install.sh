9351553a3cf5034b5850929887c64b765c68fa18
Robin Malik <rbmalik1994@gmail.com>
2024-06-04 14:05:00 +0000

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [ -z "$USER" ]; then
     USER=$(id -un)
 fi
-
+mkdir /workspaces/.codespaces/.persistedshare/dotfiles
 mv /workspaces/.codespaces/.persistedshare/dotfiles /workspaces/dotfiles/.dotfiles/bash
 mv /workspaces/.codespaces/.persistedshare/dotfiles /workspaces/dotfiles/.dotfiles/github
 mv /workspaces/.codespaces/.persistedshare/dotfiles /workspaces/dotfiles/.dotfiles/markdown
